bbef428dd422870bca0c6717081d7bd0b8482a0f
Yongli <yl.low91@gmail.com>
2023-01-20 14:16:39 +0800

    tic tac toe game

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,6 +1,6 @@
 package sg.edu.nus.iss;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -16,7 +16,8 @@ public final class App {
     public static void main(String[] args) {
         String[] tttBoard = new String[9];
         // Player X and O
-        String player = "X";
+        String player = "X";  // player to start first
+        String winner = "";
 
         // Populate the tic tac toe board with 1 to 9
         for (int i = 0; i < tttBoard.length; i++) {
@@ -30,59 +31,43 @@ public final class App {
         System.out.println("-----------------");
         System.out.println("X will play first.");
 
-        
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    public String checkWinner(String[] board) {
+        while (winner == "") {
+            // Play tic tac toe here...
+            Integer input = scanner.nextInt();
 
-        // Winners are (1,2,3), (4,5,6), (7,8,9), (1,4,7), (2,5,8), (3,6,9), (1,5,9), (3,5,7)
+            // only accept keyboad input 1 to 9
+            if (input >= 1 && input <= 9) {
 
-        String line = "";
-        String winner = "";
-
-        Integer i = 0;
-        while (i < 8) {
-            switch(i) {
-                case 0: line = board[0] + board[1] + board[2];
-                    break;
-                case 1: line = board[3] + board[4] + board[5];
-                    break;
-                case 2: line = board[6] + board[7] + board[8];
-                    break;
-                case 3: line = board[0] + board[3] + board[6];
-                    break;
-                case 4: line = board[1] + board[4] + board[7];
-                    break;
-                case 5: line = board[2] + board[5] + board[8];
-                    break;
-                case 6: line = board[0] + board[4] + board[8];
-                    break;
-                case 7: line = board[2] + board[4] + board[6];
-                    break;
-            }
-
-            i++;
-
-            if (line.equalsIgnoreCase("XXX")) {
-                winner = "X";
-                i = 9;  // kill the while loop
-            }
-            else if (line.equalsIgnoreCase("OOO")) {
-                winner = "O";
-                i = 9;  // kill the while loop
             }
             else {
-                for (int a = 0; a < 9; a++) {
-                    if (Arrays.asList(board).contains(String.valueOf(a + 1))) {
-                        break;
-                    }
-                    else if (a == 8) {
-                        winner = "draw";
-                    }
+                System.out.println("Please key in a number between 1 to 9!");
+                continue;
+            }
+
+            // assign X or O to position if its number 
+            if (tttBoard[input - 1].equals(String.valueOf(input))) {
+                tttBoard[input - 1] = player; 
+
+                // switch to another player's turn
+                if (player.equalsIgnoreCase("X")) {
+                    player = "O";
+                    System.out.println("O will play now..");
                 }
+                else {
+                    player = "X";
+                    System.out.println("X will play now..");
+                }
+            }
+
+            ttt.printBoard(tttBoard);
+            winner = ttt.checkWinner(tttBoard);
+            if (!winner.equals("")) {
+                System.out.println("Winner is " + winner);
             }
         }
 
-        return winner;
+        scanner.close();
     }
 }
--- src/main/java/sg/edu/nus/iss/TicTacToe.java
+++ src/main/java/sg/edu/nus/iss/TicTacToe.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.iss;
 
+import java.util.Arrays;
+
 public class TicTacToe {
     
     public void printBoard(String[] board) {
@@ -10,5 +12,58 @@ public class TicTacToe {
         System.out.println("-------------");
         System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
         System.out.println("|---|---|---|");
+    }
+
+    public String checkWinner(String[] board) {
+
+        // Winners are (1,2,3), (4,5,6), (7,8,9), (1,4,7), (2,5,8), (3,6,9), (1,5,9), (3,5,7)
+
+        String line = "";
+        String winner = "";
+
+        Integer i = 0;
+        while (i < 8) {
+            switch(i) {
+                case 0: line = board[0] + board[1] + board[2];
+                    break;
+                case 1: line = board[3] + board[4] + board[5];
+                    break;
+                case 2: line = board[6] + board[7] + board[8];
+                    break;
+                case 3: line = board[0] + board[3] + board[6];
+                    break;
+                case 4: line = board[1] + board[4] + board[7];
+                    break;
+                case 5: line = board[2] + board[5] + board[8];
+                    break;
+                case 6: line = board[0] + board[4] + board[8];
+                    break;
+                case 7: line = board[2] + board[4] + board[6];
+                    break;
+            }
+
+            i++;
+
+            if (line.equalsIgnoreCase("XXX")) {
+                winner = "X";
+                i = 9;  // kill the while loop
+            }
+            else if (line.equalsIgnoreCase("OOO")) {
+                winner = "O";
+                i = 9;  // kill the while loop
+            }
+            else {
+                for (int a = 0; a < 9; a++) {
+                    if (Arrays.asList(board).contains(String.valueOf(a + 1))) {
+                        break;
+                    }
+                    else if (a == 8) {
+                        winner = "draw";
+                    }
+                }
+            }
+        }
+
+        return winner;
     }
 }
